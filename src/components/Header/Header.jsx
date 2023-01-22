4dab1f7df99e75dd68284e5bdc854cc7c847415b
Alexey Grevtsov <grevold@yandex.ru>
2023-01-23 00:56:55 +0300

    Новый хедер 3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import { navigation } from "../../resources";
 import { Link } from "react-router-dom";
 
 import s from "./Header.module.css";
-
+//
 export function Header() {
   return (
     <header className={s.root}>
